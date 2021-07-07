9265ccd635f61f70a5c02f7809d81dafb9acbcf4
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-07 15:04:48 +0100

    Disable lint

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -7,7 +7,28 @@
 </template>
 
 <script>
-export default {};
-</script>
+/* eslint-disable */
 
-<style lang="scss" scoped></style>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      contacts: [],
+      name: [],
+      relationship: [],
+      errors: []
+    };
+  },
+  created() {
+    axios
+      .get("localhost:3000/contacts")
+      .then(response => {
+        this.contacts = response.data;
+      })
+      .catch(e => {
+        this.error.push(e);
+      });
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
+/* eslint-disable */
+
 import Vue from "vue";
 import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
+//import Contacts from "@/components/Contacts"
 
 Vue.use(Router);
 
