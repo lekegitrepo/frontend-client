819addf8ecd7337b04fbce410d374708873bec09
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-07 14:49:52 +0100

    Create Contacts component

---
+++ src/components/Contacts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="contacts">
+    <div v-for="contact in contacts" :key="contact.id">
+      <p>{{ contact.name }} is my {{ contact.relationship }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
